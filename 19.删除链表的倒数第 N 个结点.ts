2686c7bc93be27db5b69a63e326885a2000558d6
jasonrong <jasonrong@mapxus.com>
2025-09-18 17:03:24 +0800

    一次遍历解法 19

--- 19.删除链表的倒数第 N 个结点.ts
+++ 19.删除链表的倒数第 N 个结点.ts
@@ -19,34 +19,60 @@
  */
 
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+    // if (head === null) {
+    //     return head;
+    // }
+    // let count = 1;
+    // let curNode = head;
+    // while (curNode.next !== null) {
+    //     count++;
+    //     curNode = curNode.next;
+    // }
+    // if (count === n) {
+    //     head = head.next;
+    //     return head;
+    // }
+    // let preNode = null;
+    // let nextNode = null;
+    // let node = head;
+    // const matchIndex = count - n;
+    // for (let i = 0; i < count; i++) {
+    //     if (i === matchIndex - 1) {
+    //         // 目标节点的前一个节点
+    //         preNode = node;
+    //     } else if (i === matchIndex + 1) {
+    //         // 目标节点的下一个节点
+    //         nextNode = node;
+    //     }
+    //     node = node.next;
+    // }
+    // preNode.next = nextNode;
+    // return head;
+
+    // * 一次遍历完成
     if (head === null) {
         return head;
     }
+
+    let fastNode = head;
+    let lazyNode = head;
     let count = 1;
-    let curNode = head;
-    while (curNode.next !== null) {
-        count++;
-        curNode = curNode.next;
-    }
-    if (count === n) {
-        head = head.next;
-        return head;
-    }
-    let preNode = null;
-    let nextNode = null;
-    let node = head;
-    const matchIndex = count - n;
-    for (let i = 0; i < count; i++) {
-        if (i === matchIndex - 1) {
-            // 目标节点的前一个节点
-            preNode = node;
-        } else if (i === matchIndex + 1) {
-            // 目标节点的下一个节点
-            nextNode = node;
+    // 遍历一次链表 （遍历结束时， 懒节点会停在将要移除节点的前面）
+    while (fastNode.next !== null) {
+        // 快节点先走
+        fastNode = fastNode.next;
+        // 懒节点等待 n 次
+        if (count > n) {
+            lazyNode = lazyNode.next;
         }
-        node = node.next;
+        // 记录当前链表的长度
+        count++;
     }
-    preNode.next = nextNode;
+    // 判断删除的节点是否是当前链表的的头结点
+    if (count === n) {
+        return head.next;
+    }
+    lazyNode.next = lazyNode.next?.next || null;
     return head;
 }
 // @lc code=end
