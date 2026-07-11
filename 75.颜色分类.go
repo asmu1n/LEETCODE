2779e27ff2749adcbb7255425d9c815d11b0dfcc
asmu1n <asmuins@foxmail.com>
2026-07-11 16:15:21 +0800

    75 冒泡排序

---
+++ 75.颜色分类.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=75 lang=golang
+ * @lcpr version=
+ *
+ * [75] 颜色分类
+ */
+
+// @lc code=start
+func sortColors(nums []int) {
+	len := len(nums)
+	for i := 0; i < len-1; i++ {
+		for j := 0; j < len-1-i; j++ {
+			if nums[j] > nums[j+1] {
+				temp := nums[j]
+				nums[j] = nums[j+1]
+				nums[j+1] = temp
+			}
+		}
+	}
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [2,0,2,1,1,0]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,0,1]\n
+// @lcpr case=end
+
+*/
+
