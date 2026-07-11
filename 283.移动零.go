d86e21c7e21b02d5d05fb563d21013a07ee6e058
asmu1n <asmuins@foxmail.com>
2026-07-11 16:26:13 +0800

    283 冒泡排序

---
+++ 283.移动零.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=golang
+ * @lcpr version=
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+func moveZeroes(nums []int) {
+	len := len(nums)
+	for i := 0; i < len-1; i++ {
+		for j := 0; j < len-1-i; j++ {
+			if nums[j] == 0 && nums[j+1] != 0 {
+				temp := nums[j]
+				nums[j] = nums[j+1]
+				nums[j+1] = temp
+			}
+		}
+	}
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [0,1,0,3,12]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n
+// @lcpr case=end
+
+*/
+
